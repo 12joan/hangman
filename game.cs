7224526a76fe2ba7d90201b4501a6228c78f7abd
Joe Anderson <joejbajoe@gmail.com>
2017-07-06 19:16:57 +0100

    Give Game the GuessedLetters property
    Invisible from the outside; only used internally.

--- game.cs
+++ game.cs
@@ -1,10 +1,14 @@
 using System;
+using System.Collections.Generic;
 
 namespace Hangman {
   public class Game {
     public string Word;
+    private List<char> GuessedLetters;
+
     public Game(string word) {
       Word = word;
+      GuessedLetters = new List<char>();
     }
   }
 }
