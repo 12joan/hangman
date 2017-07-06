189207e4beb212b63771e5d46752c3bc1d0a89b4
Joe Anderson <coderdojoe@wodniw.local>
2017-07-06 18:33:17 +0100

    Enclose the Hangman class in a namespace

--- hangman.cs
+++ hangman.cs
@@ -1,14 +1,16 @@
 using System;
 using Table;
 
-public class Hangman {
-  public static void Main(string[] args) {
-    Table.Table t = new Table.Table(2, 3);
-    Console.WriteLine("Hello, World!");
-    Console.WriteLine("You entered the following {0} command line arguments:",
-         args.Length );
-    for (int i=0; i < args.Length; i++) {
-      Console.WriteLine("{0}", args[i]); 
+namespace Hangman {
+  public class Hangman {
+    public static void Main(string[] args) {
+      Table.Table t = new Table.Table(2, 3);
+      Console.WriteLine("Hello, World!");
+      Console.WriteLine("You entered the following {0} command line arguments:",
+           args.Length );
+      for (int i=0; i < args.Length; i++) {
+        Console.WriteLine("{0}", args[i]); 
+      }
     }
   }
 }
