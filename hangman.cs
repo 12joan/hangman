ca303ca3725026d89cd2438121a34e7b488ee856
Joe Anderson <coderdojoe@wodniw.local>
2017-07-06 23:49:58 +0100

    Comment out game logic and replace table code
    Time to set up the game layout.

--- hangman.cs
+++ hangman.cs
@@ -3,19 +3,19 @@ using System;
 namespace Hangman {
   public class Hangman {
     public static void Main(string[] args) {
-      char key = Console.ReadKey(true).KeyChar;
+      // char key = Console.ReadKey(true).KeyChar;
 
-      var game = new Game("HANG THE MAN");
-      bool wasCorrect = game.GuessLetter(key);
-      Console.WriteLine(wasCorrect.ToString());
+      // var game = new Game("HANG THE MAN");
+      // bool wasCorrect = game.GuessLetter(key);
+      // Console.WriteLine(wasCorrect.ToString());
 
-      var output = game.ShownWord();
-      Console.WriteLine(output);
-
-      // Table table = new Table(2, 3);
-      // string output = table.Draw();
-
+      // var output = game.ShownWord();
       // Console.WriteLine(output);
+
+      Table table = new Table(2, 3);
+      string output = table.Draw();
+
+      Console.WriteLine(output);
     }
   }
 }
