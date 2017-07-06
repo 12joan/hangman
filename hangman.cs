48b3b3b64024641674244028431229db7a44fe20
Joe Anderson <joejbajoe@gmail.com>
2017-07-06 19:07:43 +0100

    Create an instance of Game

--- hangman.cs
+++ hangman.cs
@@ -3,6 +3,9 @@ using System;
 namespace Hangman {
   public class Hangman {
     public static void Main(string[] args) {
+      Game game = new Game();
+      Console.WriteLine("Made a new Game object");
+
       // Table table = new Table(2, 3);
       // string output = table.Draw();
 
