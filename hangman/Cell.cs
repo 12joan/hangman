9118c9847e79bf9f13b2d40f19961d218cb30546
Joe Anderson <coderdojoe@wodniw.local>
2017-07-09 10:02:14 +0100

    Move important files into new project

---
+++ hangman/Cell.cs
@@ -0,0 +1,59 @@
+using System;
+
+namespace Hangman {
+  public class Cell {
+    public const int RightAlign  = 0;
+    public const int CentreAlign = 1;
+    public const int LeftAlign   = 2;
+
+    public string Text;
+    public int Align;
+
+    public Cell(string text) {
+      Text = text;
+      Align = LeftAlign;
+    }
+
+    public Cell(string text, int align) {
+      Text = text;
+      Align = align;
+    }
+
+    public int LeftMargin(int width) {
+      switch (Align) {
+        case LeftAlign:
+          return 0;
+        case CentreAlign:
+          return (width - Length()) / 2;
+        case RightAlign:
+          return width - Length();
+        default:
+          return -1; // should never reach
+      }
+    }
+
+    public int Length() {
+      int max = 0;
+      foreach (var line in Lines()) {
+        max = Math.Max(max, line.Length);
+      }
+      return max;
+    }
+
+    public string[] Lines() {
+      return Text.Split('\n');
+    }
+
+    public string LineAtIndex(int index, string notFound="") {
+      if (index < Lines().Length) {
+        return Lines()[index];
+      } else {
+        return notFound;
+      }
+    }
+
+    public int Depth() {
+      return Lines().Length;
+    }
+  }
+}
--- hangman/Program.cs
+++ hangman/Program.cs
@@ -1,12 +1,53 @@
-﻿using System;
+using System;
+using System.IO;
 
-namespace hangman
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
+namespace Hangman {
+  public class Hangman {
+    public static void Main(string[] args) {
+      // char key = Console.ReadKey(true).KeyChar;
+
+      // var game = new Game("HANG THE MAN");
+      // bool wasCorrect = game.GuessLetter(key);
+      // Console.WriteLine(wasCorrect.ToString());
+
+      // var output = game.ShownWord();
+      // Console.WriteLine(output);
+
+      string titleText = File.ReadAllText("title.txt");
+
+      Cell[] title = {
+        new Cell(titleText, Cell.CentreAlign)
+      };
+
+      Cell[] word = {
+        new Cell("_E__O _O___", Cell.CentreAlign)
+      };
+
+      Cell[] stats = {
+        new Cell("Incorrect letters:\n A B I U"),
+        new Cell("Lives remaining:\n 11/15", Cell.RightAlign)
+      };
+
+      Cell[] status = {
+        new Cell("Press any letter to guess!", Cell.CentreAlign)
+      };
+
+      Row[] rows = {
+        new Row(title),
+        new Row(word),
+        new Row(stats),
+        new Row(status)
+      };
+
+      var table = new Table(
+        Math.Min(81, Console.WindowWidth),
+        2,
+        rows
+      );
+
+      var output = table.Draw();
+
+      Console.WriteLine(output);
     }
+  }
 }
