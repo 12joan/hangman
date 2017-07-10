9fedc79f282c22d070635dd2d34bf9af59ea59dc
Joe Anderson <joejbajoe@gmail.com>
2017-07-10 22:13:57 +0100

    Loop char input

--- Program.cs
+++ Program.cs
@@ -4,47 +4,50 @@ using System.IO;
 namespace Hangman {
   public class Hangman {
     public static void Main(string[] args) {
-      char key = Console.ReadKey(true).KeyChar;
-
       var game = new Game("HANG THE MAN");
-      bool wasCorrect = game.GuessLetter(key);
 
-      string titleText = File.ReadAllText("title.txt");
+      while (true) {
+        string titleText = File.ReadAllText("title.txt");
 
-      Cell[] title = {
-        new Cell(titleText, Cell.CentreAlign)
-      };
+        Cell[] title = {
+          new Cell(titleText, Cell.CentreAlign)
+        };
 
-      string shownWord = game.ShownWord();
-      Cell[] word = {
-        new Cell(shownWord, Cell.CentreAlign)
-      };
+        string shownWord = game.ShownWord();
+        Cell[] word = {
+          new Cell(shownWord, Cell.CentreAlign)
+        };
 
-      Cell[] stats = {
-        new Cell("Incorrect letters:\n A B I U"),
-        new Cell("Lives remaining:\n 11/15", Cell.RightAlign)
-      };
+        Cell[] stats = {
+          new Cell("Incorrect letters:\n A B I U"),
+          new Cell("Lives remaining:\n 11/15", Cell.RightAlign)
+        };
 
-      Cell[] status = {
-        new Cell("Press any letter to guess!", Cell.CentreAlign)
-      };
+        Cell[] status = {
+          new Cell("Press any letter to guess!", Cell.CentreAlign)
+        };
 
-      Row[] rows = {
-        new Row(title),
-        new Row(word),
-        new Row(stats),
-        new Row(status)
-      };
+        Row[] rows = {
+          new Row(title),
+          new Row(word),
+          new Row(stats),
+          new Row(status)
+        };
 
-      var table = new Table(
-        Math.Min(81, Console.WindowWidth),
-        2,
-        rows
-      );
+        var table = new Table(
+          Math.Min(81, Console.WindowWidth),
+          2,
+          rows
+        );
 
-      var tableOutput = table.Draw();
+        var tableOutput = table.Draw();
 
-      Console.WriteLine(tableOutput);
+        Console.WriteLine(tableOutput);
+
+        char key = Console.ReadKey(true).KeyChar;
+        bool wasCorrect = game.GuessLetter(key);
+        Console.Clear();
+      }
     }
   }
 }
