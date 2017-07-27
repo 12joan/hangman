52d30180216747508f937ad2b93d4f8aa0c5af05
Joe Anderson <joejbajoe@gmail.com>
2017-07-27 11:53:43 +0100

    Map configurations to empty rows

--- TableFactory.cs
+++ TableFactory.cs
@@ -1,12 +1,20 @@
 using System;
 using System.Collections;
+using System.Collections.Generic;
 
 namespace Hangman {
   public class TableFactory {
     public static Table Build(object[] config) {
-      Row[] rows = {
-      };
-      return new Table(1, 2, rows);
+      var rows = new List<Row>();
+      foreach (object[] rowConfig in config) {
+        rows.Add(BuildRow(rowConfig));
+      }
+      return new Table(1, 2, rows.ToArray());
+    }
+
+    public static Row BuildRow(object[] rowConfig) {
+      Cell[] cells = {};
+      return new Row(cells);
     }
   }
 }
