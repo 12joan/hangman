61ed49982e201d3a25cb7746c74f873634638e84
Joe Anderson <coderdojoe@wodniw.local>
2017-08-11 15:37:28 +0100

    Randomise words separated by newline

--- RandomWord.cs
+++ RandomWord.cs
@@ -4,11 +4,19 @@ using System.IO;
 namespace Hangman {
   public class RandomWord : IWordable {
     public string Word() {
-      return RawWords();
+      return Words()[Position()].ToUpper();
+    }
+
+    private int Position() {
+      return (new Random()).Next(Words().Length);
+    }
+
+    private string[] Words(){
+      return RawWords().Split(new string[] { Environment.NewLine }, StringSplitOptions.None);
     }
 
     private string RawWords() {
-      return File.ReadAllText("words.txt");
+      return File.ReadAllText("words.txt").TrimEnd(Environment.NewLine.ToCharArray());
     }
   }
 }
